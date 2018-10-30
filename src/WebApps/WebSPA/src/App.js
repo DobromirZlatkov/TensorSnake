f3173e7ba0f3e397d146a5bdeb5cf6978d2adbf8
Dobromir Zlatkov <dobromir@twogears.com>
2018-10-30 13:43:28 +0200

    Create snake game file. Add some css.

--- src/WebApps/WebSPA/src/App.js
+++ src/WebApps/WebSPA/src/App.js
@@ -1,45 +1,36 @@
 import React, { Component } from 'react';
-import ClientOauth2 from 'client-oauth2';
-import logo from './logo.svg';
 import './App.css';
-
+import SnakeGame from './snake'
 
 class App extends Component {
 
     componentDidMount() {
-        console.log('fasdfasdfasfsda')
-        var githubAuth = new ClientOauth2({
-            clientId: 'spa',
-            clientSecret: 'secret',//'K7gNU3sdo+OL0wNhqoVWhr3g6s1xYv72ol/pe/Unols=',
-            accessTokenUri: 'http://localhost:3105/connect/token',
-            authorizationUri: 'http://localhost:3105/connect/authorize',
-            scopes: ["openid", "profile", "offline_access"]
-        });
-
-        githubAuth.owner.getToken('demouser@test.com', 'Pass@word1')
-            .then(function (user) {
-                console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... }
-            })
+        console.log(SnakeGame, "SnakeGame")
     }
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                  </a>
-                </header>
-            </div>
+            //<div className="App">
+            //    <header className="App-header">
+            //        <img src={logo} className="App-logo" alt="logo" />
+            //        <p>
+            //            Edit <code>src/App.js</code> and save to reload.
+            //      </p>
+            //        <a
+            //            className="App-link"
+            //            href="https://reactjs.org"
+            //            target="_blank"
+            //            rel="noopener noreferrer"
+            //        >
+            //            Learn React
+            //      </a>
+            //    </header>
+            //</div>
+
+            <body>
+                <div className="score" id="score">0</div>
+                <canvas id="canvas" height="500px" width="500px"></canvas>
+            </body>
         );
     }
 }
